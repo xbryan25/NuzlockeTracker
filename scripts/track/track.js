cd89a991755fd1b422b51d78af69cd3417b38b29
Bryan <bryanagan25@gmail.com>
2024-08-25 20:37:27 +0800

    Add multiple location rows; generate location rows using JS

--- scripts/track/track.js
+++ scripts/track/track.js
@@ -4,3 +4,71 @@ let userDecision = localStorage.getItem('userDecision');
 
 document.querySelector('.js-heading-text').innerHTML = `Pokemon ${userDecision}`;
 
+let sampleRoutes = [{
+  location: 'Route 1',
+  availablePokemon: ['Charmander', 'Squirtle', 'Bulbasaur']
+}, {
+  location: 'Route 2',
+  availablePokemon: ['Pidgey', 'Rattata', 'Mankey']
+}]
+
+let entireHTML = '';
+
+
+sampleRoutes.forEach((sampleRoute) => {
+  entireHTML += `<div class="location-row js-location-row">
+                  <p class="location-text">${sampleRoute.location}</p>
+                  <select name="Route 1" class="every-combobox">
+                    <option value="volvo">Volvo</option>
+                  </select>
+
+                  <select name="Route 1" class="every-combobox">
+                    ${availablePokemonHTMLCreator(sampleRoute.availablePokemon)}
+                  </select>
+
+                  <select name="Route 1" class="every-combobox">
+                    <option value="volvo">Chagra</option>
+                  </select>
+
+                  <select name="Route 1" class="every-combobox">
+                    <option value="volvo">Chagra</option>
+                  </select>
+              </div>`;
+});
+
+function availablePokemonHTMLCreator(availablePokemon){
+  let availablePokemonHTML = '<option value="none" selected disabled hidden>Find encounter</option>';
+
+  availablePokemon.forEach((pokemon) => {
+    availablePokemonHTML += `<option value="${pokemon}">${pokemon}</option>`
+  });
+
+  return availablePokemonHTML;
+}
+
+
+
+// document.querySelector('.js-center-box')
+//     .innerHTML = `<p class="location-text">Route 1</p>
+//                 <select name="Route 1" class="every-combobox">
+//                     <option value="volvo">Volvo</option>
+//                 </select>
+
+//                 <select name="Route 1" class="every-combobox">
+//                         <option value="volvo">Chagra</option>
+//                 </select>
+
+//                 <select name="Route 1" class="every-combobox">
+//                         <option value="volvo">Chagra</option>
+//                 </select>
+
+//                 <select name="Route 1" class="every-combobox">
+//                         <option value="volvo">Chagra</option>
+//                 </select>
+                
+//                 `;
+
+document.querySelector('.js-center-box-container')
+    .innerHTML = entireHTML;
+
+
