a45c6fe5be6aa5b49d9399f5bea760d276de90ce
Bryan <bryanagan25@gmail.com>
2024-08-25 14:01:47 +0800

    Change classlist.toggle to .add/.remove for better readability

--- scripts/front-page/pop-up.js
+++ scripts/front-page/pop-up.js
@@ -2,10 +2,11 @@ const supportedGamesButton = document.querySelector('.js-supported-games');
 const popUpExitButton = document.querySelector('.js-close-btn');
 
 supportedGamesButton.addEventListener('click', () => {
-    document.getElementById("js-popup-1").classList.toggle("active");
+    // set .active to on
+    document.getElementById("js-popup-1").classList.add("active");
 });
 
 popUpExitButton.addEventListener('click', () => {
-    document.getElementById("js-popup-1").classList.toggle("active");
-    // alert('pressed');
+    // set .active to off
+    document.getElementById("js-popup-1").classList.remove("active");
 })
